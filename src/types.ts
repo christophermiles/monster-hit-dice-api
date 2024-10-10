aa59952d31ac380e3349700e0c7dd4369dfa3397
Christoper Miles <mail@christophermiles.com.au>
2024-10-10 18:20:25 +1100

    Tidy up

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-import type { RouteConfig, RouteHandler } from '@hono/zod-openapi'
 import type { HitPointResults } from 'roll-hit-dice/dist/types'
 
 export interface HitPointResultsResponseItem {
@@ -8,17 +7,6 @@ export interface HitPointResultsResponseItem {
 
 export type HitPointResultsResponse = HitPointResultsResponseItem[]
 
-export type HitPointByMonsterNameResultsResponseItem = HitPointResultsResponseItem & {
-  monster: {
-    name: string
-    source: string
-  }
-}
-
-export type HitPointByMonsterNameResultsResponse = HitPointByMonsterNameResultsResponseItem[]
-
 export interface Variables {
   hitDiceExpressions: string[]
 }
-
-export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R>

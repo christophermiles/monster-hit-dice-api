3853ca193203c946e39f7570e9f5177705a329af
Christoper Miles <mail@christophermiles.com.au>
2024-10-10 11:47:57 +1100

    Avoid annoying linting issue

--- src/index.ts
+++ src/index.ts
@@ -22,26 +22,28 @@ app.doc('/doc', {
   info: {
     version: '1.0.0',
     title: 'Hit Dice App API',
-    description: 'An API for calculating the following Hit Point values from a [Dungeons &amp; Dragons creature’s Hit Dice expression](https://www.dndbeyond.com/sources/dnd/basic-rules-2014/monsters#HitPoints):\n\n'
-      + '- **minimum** (lowest possible Hit Die roll, plus modifier)\n\n'
-      + '- **weak** (midway between minimum and average)\n\n'
-      + '- **average** (as listed in the stat block)\n\n'
-      + '- **strong** (midway between average and maximum)\n\n'
-      + '- **maximum** (highest possible Hit Die roll, plus modifier)\n\n'
-      + '## Why?\n\n'
-      + 'Using a creature’s average Hit Points works just fine for most combats.\n\n'
-      + 'But sometimes you might want to give your <abbr title="Big Bad Evil Guy">BBEG</abbr> (Big Bad Evil Guy) maximum Hit Points&nbsp;😈.\n\n'
-      + 'Or maybe you’re using multiple creatures of the same in a battle, and want them to have varying degrees of durability.\n\n'
-          + 'Or you want to tweak a creature’s Hit Points mid-battle ([Mike Shea at Sly Flourish explains why we might want to do this](https://slyflourish.com/tweaking_monster_hit_points.html)), but don’t feel comfortable choosing a *completely* arbitrary number.\n\n'
-            + 'These endpoints allow you to submit a Hit Dice expression&hellip;\n\n'
-            + '- `2d8-2` (Skeleton)\n\n'
-            + '&hellip;or even multiple Hit Dice expressions at once&hellip;\n\n'
-            + '- `2d8-2` (Skeleton)\n\n'
-            + '- `2d8+6` (Orc)\n\n'
-            + '- `8d10+40` (Gelatinous Cube)\n\n'
-            + '- `33d20+330` (Tarrasque)\n\n'
-            + '&hellip;and get back an array of Hit Point values.\n\n'
-            + 'See the documentation of the individual endpoints for more details.',
+    description: [
+      'An API for calculating the following Hit Point values from a [Dungeons &amp; Dragons creature’s Hit Dice expression](https://www.dndbeyond.com/sources/dnd/basic-rules-2014/monsters#HitPoints):',
+      '- **minimum** (lowest possible Hit Die roll, plus modifier)',
+      '- **weak** (midway between minimum and average)',
+      '- **average** (as listed in the stat block)',
+      '- **strong** (midway between average and maximum)',
+      '- **maximum** (highest possible Hit Die roll, plus modifier)',
+      '## Why?',
+      'Using a creature’s average Hit Points works just fine for most combats.',
+      'But sometimes you might want to give your <abbr title="Big Bad Evil Guy">BBEG</abbr> (Big Bad Evil Guy) maximum Hit Points&nbsp;😈.',
+      'Or maybe you’re using multiple creatures of the same in a battle, and want them to have varying degrees of durability.',
+      'Or you want to tweak a creature’s Hit Points mid-battle ([Mike Shea at Sly Flourish explains why we might want to do this](https://slyflourish.com/tweaking_monster_hit_points.html)), but don’t feel comfortable choosing a *completely* arbitrary number.',
+      'These endpoints allow you to submit a Hit Dice expression&hellip;',
+      '- `2d8-2` (Skeleton)',
+      '&hellip;or even multiple Hit Dice expressions at once&hellip;',
+      '- `2d8-2` (Skeleton)',
+      '- `2d8+6` (Orc)',
+      '- `8d10+40` (Gelatinous Cube)',
+      '- `33d20+330` (Tarrasque)',
+      '&hellip;and get back an array of Hit Point values.',
+      'See the documentation of the individual endpoints for more details.',
+    ].join('\n\n'),
   },
 })
 

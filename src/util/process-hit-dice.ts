0b5287c1a4fe20adc83847b6d1d92bb717c98ac0
Christoper Miles <mail@christophermiles.com.au>
2024-10-12 20:27:12 +1100

    Update to use new version of roll-hit-dice

--- src/util/process-hit-dice.ts
+++ src/util/process-hit-dice.ts
@@ -1,13 +1,11 @@
 import type { HitPointResultsResponse, HitPointResultsResponseItem } from '../types'
 import rollHitDice from 'roll-hit-dice/dist/roll-hit-dice'
-import parseHitDice from './parse-hit-dice-expression-string'
 
 export function processHitDice(expressions: string[]): HitPointResultsResponse {
-  return expressions.map((expression): HitPointResultsResponseItem => {
-    const parsedHitDice = parseHitDice(expression)
+  return expressions.map((hitDice): HitPointResultsResponseItem => {
     return {
-      hitDice: expression,
-      hitPoints: rollHitDice(parsedHitDice.diceCount, parsedHitDice.dieType, parsedHitDice.modifier),
+      hitDice,
+      hitPoints: rollHitDice(hitDice),
     }
   })
 }

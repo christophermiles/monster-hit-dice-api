c27c0c1b320307beb39bded3066df10fc5d0345f
Christoper Miles <mail@christophermiles.com.au>
2024-10-13 15:16:31 +1100

    Added dice emoji to invalid Hit Dice message

--- src/constants.ts
+++ src/constants.ts
@@ -9,4 +9,4 @@ import { HIT_DICE_BY_MONSTER } from './mocks'
 
 export const CSV_RESPONSE_HEADER_ROW = 'Hit Dice,Minimum,Weak,Average,Strong,Maximum'
 
-export const INVALID_HIT_DICE_ERROR_MESSAGE = `hd must be a valid Hit Dice expression such as ${Object.values(HIT_DICE_BY_MONSTER).map(hd => hd).join(', ')}`
+export const INVALID_HIT_DICE_ERROR_MESSAGE = `🎲 hd must be a valid Hit Dice expression such as ${Object.values(HIT_DICE_BY_MONSTER).map(hd => hd).join(', ')}`
